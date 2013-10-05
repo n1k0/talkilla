b9e1f5c10a37db2d375f1f79e5f51ae78f5597ef
Nicolas Perriault <nperriault@gmail.com>
2013-10-05 15:31:47 +0200

    extracted DB tests from CollectedContacts ones

--- test/frontend/addressbook/collected_test.js
+++ test/frontend/addressbook/collected_test.js
@@ -1,4 +1,4 @@
-/*global sinon, chai, CollectedContacts, IDBDatabase, IDBObjectStore */
+/*global sinon, chai, CollectedContacts, IDBObjectStore */
 /* jshint expr:true */
 
 var expect = chai.expect;
@@ -30,41 +30,6 @@ describe("CollectedContacts", function() {
         "dbname", "storename", "version");
       expect(contactsDb.options.storename).eql("contacts");
       expect(contactsDb.options.version).eql(1);
-    });
-  });
-
-  describe("#load", function() {
-    it("should load the database", function(done) {
-      contactsDb.load(function(err, db) {
-        expect(err).to.be.a("null");
-        expect(db).to.be.an.instanceOf(IDBDatabase);
-        expect(contactsDb.db).to.be.an.instanceOf(IDBDatabase);
-        expect(contactsDb.db).to.deep.equal(db);
-        done();
-      });
-    });
-
-    it("shouldn't throw if the database is already loaded", function(done) {
-      contactsDb.load(function(db1) {
-        contactsDb.load(function(db2) {
-          expect(db1).eql(db2);
-          done();
-        });
-      });
-    });
-
-    it("should pass back any encountered error", function(done) {
-      sandbox.stub(indexedDB, "open", function() {
-        var request = {};
-        setTimeout(function() {
-          request.onerror({target: {errorCode: "load error"}});
-        });
-        return request;
-      });
-      contactsDb.load(function(err) {
-        expect(err).eql("load error");
-        done();
-      });
     });
   });
 
@@ -150,41 +115,6 @@ describe("CollectedContacts", function() {
       });
       contactsDb.allUsernames(function(err) {
         expect(err).eql("all error");
-        done();
-      });
-    });
-  });
-
-  describe("#close", function() {
-    it("should close the database", function() {
-      contactsDb.close();
-      expect(contactsDb.db).to.be.a("undefined");
-    });
-  });
-
-  describe("#drop", function() {
-    it("should drop the database", function(done) {
-      contactsDb.addUsername("niko", function() {
-        this.drop(function(err) {
-          expect(err).to.be.a("null");
-          this.allUsernames(function(err, contacts) {
-            expect(contacts).to.have.length.of(0);
-            done();
-          });
-        });
-      });
-    });
-
-    it("should pass back any encountered error", function(done) {
-      sandbox.stub(indexedDB, "deleteDatabase", function() {
-        var request = {};
-        setTimeout(function() {
-          request.onerror({target: {errorCode: "drop error"}});
-        });
-        return request;
-      });
-      contactsDb.drop(function(err) {
-        expect(err).eql("drop error");
         done();
       });
     });
---
+++ test/frontend/addressbook/db_test.js
@@ -0,0 +1,257 @@
+/*global sinon, chai, DB, IDBDatabase, IDBObjectStore, xit */
+/* jshint expr:true */
+
+var expect = chai.expect;
+
+describe("DB", function() {
+  var sandbox;
+
+  function createTestDB(options, _createStore) {
+    function TestDB() {
+      DB.apply(this, arguments);
+    }
+    TestDB.prototype = Object.create(DB.prototype);
+    TestDB.prototype._createStore = _createStore;
+    return new TestDB(options);
+  }
+
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  describe("#constructor", function() {
+    it("should construct an object", function() {
+      var testDB = createTestDB({
+        dbname: "test",
+        storename: "test"
+      }, function(){});
+      expect(testDB).to.be.a("object");
+    });
+
+    it("should raise an error if no dbname option is passed", function() {
+      expect(createTestDB.bind(null, {storename: "test"})).to.Throw(Error);
+    });
+
+    it("should raise an error if no storename option is passed", function() {
+      expect(createTestDB.bind(null, {dbname: "test"})).to.Throw(Error);
+    });
+
+    it("should set default options", function() {
+      var testDB = createTestDB({dbname: "foo", storename: "bar", version: 2});
+      expect(testDB.options).to.include.keys(
+        "dbname", "storename", "version");
+      expect(testDB.options.dbname).eql("foo");
+      expect(testDB.options.storename).eql("bar");
+      expect(testDB.options.version).eql(2);
+    });
+  });
+
+  describe("constructed", function() {
+    var testDB;
+
+    beforeEach(function() {
+      testDB = createTestDB({dbname: "test", storename: "test"}, function(db) {
+        var store = db.createObjectStore(this.options.storename, {
+          keyPath: "foo"
+        });
+        store.createIndex("foo", "foo", {unique: true});
+      });
+    });
+
+    afterEach(function(done) {
+      testDB.drop(function() {
+        done();
+      });
+    });
+
+    describe("#load", function() {
+      xit("should raise an error if no schema creation method is provided");
+
+      it("should load the database", function(done) {
+        testDB.load(function(err, db) {
+          expect(err).to.be.a("null");
+          expect(db).to.be.an.instanceOf(IDBDatabase);
+          expect(testDB.db).to.be.an.instanceOf(IDBDatabase);
+          expect(testDB.db).to.deep.equal(db);
+          done();
+        });
+      });
+
+      it("shouldn't throw if the database is already loaded", function(done) {
+        testDB.load(function(db1) {
+          testDB.load(function(db2) {
+            expect(db1).eql(db2);
+            done();
+          });
+        });
+      });
+
+      it("should pass back any encountered error", function(done) {
+        sandbox.stub(indexedDB, "open", function() {
+          var request = {};
+          setTimeout(function() {
+            request.onerror({target: {errorCode: "load error"}});
+          });
+          return request;
+        });
+        testDB.load(function(err) {
+          expect(err).eql("load error");
+          done();
+        });
+      });
+    });
+
+    describe("#add", function() {
+      it("should add a record to the database", function(done) {
+        var test = {foo: "bar"};
+        testDB.add(test, function(err, record) {
+          expect(err).to.be.a("null");
+          expect(record).eql(test);
+          this.all(function(err, records) {
+            expect(records).eql([test]);
+            done();
+          });
+        });
+      });
+
+      it("shouldn't raise an error in case of a duplicate record if the " +
+         "`silentConstraint` option is enabled",
+        function(done) {
+          var test = {foo: "bar"};
+          testDB.add(test, function(err) {
+            expect(err).to.be.a("null");
+            this.add(test, function(err) {
+              expect(err).to.be.a("null");
+              done();
+            }, {silentConstraint: true});
+          });
+        });
+
+      it("should pass back any load error", function(done) {
+        sandbox.stub(indexedDB, "open", function() {
+          var request = {};
+          setTimeout(function() {
+            request.onerror({target: {errorCode: "load error"}});
+          });
+          return request;
+        });
+        testDB.add({foo: "bar"}, function(err) {
+          expect(err).eql("load error");
+          done();
+        });
+      });
+
+      it("should pass back any add error", function(done) {
+        var test = {foo: "bar"};
+        sandbox.stub(IDBObjectStore.prototype, "add", function() {
+          var request = {};
+          setTimeout(function() {
+            request.onerror({target: {error: {name: "InvalidStateError",
+                                              message: "add error"}}});
+          });
+          return request;
+        });
+        testDB.add(test, function(err) {
+          expect(err.message).eql("add error");
+          done();
+        });
+      });
+    });
+
+    describe("#all", function() {
+      it("should retrieve no record when db is empty", function(done) {
+        testDB.all(function(err, records) {
+          expect(records).to.have.length.of(0);
+          done();
+        });
+      });
+
+      it("should retrieve all records in inserted order", function(done) {
+        var test1 = {foo: "bar"};
+        var test2 = {foo: "baz"};
+        testDB.add(test1, function() {
+          this.add(test2, function() {
+            this.all(function(err, records) {
+              expect(err).to.be.a("null");
+              expect(records).to.have.length.of(2);
+              // XXX: chai doesn't seem to be able to check if an array contains
+              // an object:
+              // expect([{a: 1}, {b: 2}]).to.contain({a: 1}) //=> AssertionError
+              var mapped = records.map(function(record) {return record.foo;});
+              expect(mapped).to.contain(test1.foo);
+              expect(mapped).to.contain(test2.foo);
+              done();
+            });
+          });
+        });
+      });
+
+      it("should pass back any load error", function(done) {
+        sandbox.stub(indexedDB, "open", function() {
+          var request = {};
+          setTimeout(function() {
+            request.onerror({target: {errorCode: "load error"}});
+          });
+          return request;
+        });
+        testDB.all(function(err) {
+          expect(err).eql("load error");
+          done();
+        });
+      });
+
+      it("should pass back any encountered error", function(done) {
+        sandbox.stub(IDBObjectStore.prototype, "openCursor", function() {
+          var cursor = {};
+          setTimeout(function() {
+            cursor.onerror({target: {errorCode: "all error"}});
+          });
+          return cursor;
+        });
+        testDB.all(function(err) {
+          expect(err).eql("all error");
+          done();
+        });
+      });
+    });
+
+    describe("#close", function() {
+      it("should close the database", function() {
+        testDB.close();
+        expect(testDB.db).to.be.a("undefined");
+      });
+    });
+
+    describe("#drop", function() {
+      it("should drop the database", function(done) {
+        testDB.add({foo: "bar"}, function() {
+          this.drop(function(err) {
+            expect(err).to.be.a("null");
+            this.all(function(err, records) {
+              expect(records).to.have.length.of(0);
+              done();
+            });
+          });
+        });
+      });
+
+      it("should pass back any encountered error", function(done) {
+        sandbox.stub(indexedDB, "deleteDatabase", function() {
+          var request = {};
+          setTimeout(function() {
+            request.onerror({target: {errorCode: "drop error"}});
+          });
+          return request;
+        });
+        testDB.drop(function(err) {
+          expect(err).eql("drop error");
+          done();
+        });
+      });
+    });
+  });
+});
